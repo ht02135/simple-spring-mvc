fe78752c71da14af006e17ffe1915c6d441a454f
Hung En Tsai <ht02135@yahoo.com>
2025-07-02 09:49:11 -0400

    commit

---
+++ src/main/webapp/lab1/lesson3/index.js
@@ -0,0 +1,35 @@
+
+function AppViewModel() {
+	//-----------------------------------------------------------------
+	// Declaration
+	var self = this;	//this is ViewModel
+
+	self.people = ko.observableArray([ 			//foreach binding
+		{ name: 'Bert', city: null },
+	    { name: 'Charles', city: "Boston" },
+	    { name: 'Denise', city: "Chicago"  }
+	]);
+	
+	self.display = ko.observable(false);	//checkbox checked binding
+
+	//-----------------------------------------------------------------
+	// Operations
+
+	self.addPerson = function() {				//a link click bindinig
+		console.log("called addPerson");
+		self.people.push({ name: "New at " + new Date() });
+	};
+
+	/*
+	pass in $data which in this case is an entry of people array?
+	*/
+	self.removePerson = function(person) {		//button click bindinig
+		console.log("called removePerson person="+person);
+		if (self.people.indexOf(person) >= 0) {
+			console.log("remove person().name="+person.name);
+			self.people.remove(person);
+		}
+	}
+}
+
+ko.applyBindings(new AppViewModel());
