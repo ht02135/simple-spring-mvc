b0548c9ec759726af8412ad011da7fa29553e1dd
Hung En Tsai <ht02135@yahoo.com>
2025-07-03 09:28:40 -0400

    commit

--- src/main/webapp/lab1/options/index.js
+++ src/main/webapp/lab1/options/index.js
@@ -13,13 +13,13 @@ function CountryViewModel() {
     var self = this;
 	
 	self.availableCountries2 = ko.observableArray(['France', 'Germany', 'Spain'])
+	self.selectedCountry2 = ko.observable();
 	
 	self.availableCountries = ko.observableArray([
 		new Country("UK", 65000000),
 		new Country("USA", 320000000),
 		new Country("Sweden", 29000000)
 	]);
-	
 	self.selectedCountry = ko.observable();
 	
 	//-----------------------------------------------------------------
